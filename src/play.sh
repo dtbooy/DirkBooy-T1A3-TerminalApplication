bfd6f8591141779a76332e675cec87b3312c791c
dtbooy <dtbooy@gmail.com>
2023-10-29 11:17:01 +1000

    updated play.sh, added commentary - de-commented code

--- src/play.sh
+++ src/play.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 1. Check for python3
+echo Checking for Python3...
 if ! [[ -x "$(command -v python3)" ]]
 then
   echo 'Error: 
@@ -7,21 +8,29 @@ then
     For a guide to installing Python 3 see: https://docs.python-guide.org/starting/installation/' >&2
   exit 1
 fi
+echo Done!
 
-# #2. install venv in quiz folder
-# echo "Activating Python virtual environment"
-# python3 -m venv .venv
-# source .venv/bin/activate
+# #2. Activate venv in quiz folder
+echo "Activating Python virtual environment"
+python3 -m venv .venv
+source .venv/bin/activate
+echo Done!
 
 # #3. install required packages from requirements.txt
-# echo "Installing required packages from requirements.txt"
-# pip install -r requirements.txt
+echo "Checking for required packages from requirements.txt"
+pip install -r requirements.txt
+echo Done!
 
 # 4. Make sure quiz files are in read-write mode 
+echo Checking 
 chmod 666 ./quiz_data/quiz_*
 
 #5. Open the quiz program
+echo Opening Application
 python3 main.py
 
 # #6. Deactivate virtual environment
-# deactivate
+echo Deactivating Virtual Environment...
+deactivate
+echo Done!
+echo Play again soon!
